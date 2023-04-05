c96972dd7d64f1ba5e78c56dfff738d014d95ffa
Saad Saiyed <saadsaiyed7@gmail.com>
2023-04-05 07:02:41 -0400

    Adding 'smapleKeys.js' to seek expected creadentials

---
+++ config/sampleKeys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID: 'somenumber-somehax.apps.googleusercontent.com',
+    googleClientSecret: 'moreComplexNumberHere',
+    mongoURI: 'mongodb+srv://username:passcode@cluster0.randStuff.mongodb.net/DBName?retryWrites=true&w=majority',
+    maxCookieAge: 12 * 24 * 60 * 60 * 1000,
+    cookieKey: 'someStupidCookiesIKnowIDontEvenLikeCookies'
+};
